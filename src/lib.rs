2d96c2bfa42acef74db6981b7b1aff975013518f
oonxt <oonxt@msn.com>
2025-01-11 12:09:38 +0800

    init commit

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,20 @@ pub enum Error {
     ParseError(#[from] binrw::Error)
 }
 pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+pub struct Image {
+    pub crc32: u32,
+    pub dimensions: (u32, u32),
+    pub width: Length,
+    pub height: Length,
+    pub x_dpi: u32,
+    pub y_dpi: u32,
+    pub content_type: String,
+    pub ext: String,
+}
 
 #[derive(Debug)]
-pub enum Image {
+pub enum ImageType {
     Bmp(Bmp),
     Gif(Gif),
     Jpeg(Jpeg),
@@ -35,88 +46,101 @@ pub enum Image {
 }
 
 impl Image {
-
     pub fn from_file(path: &str) -> Result<Image> {
         let ext = path.clone().split('.').last().ok_or(Error::CommonError("文件名不正确".to_string()))?.to_lowercase();
-
-        let mut reader = Cursor::new(std::fs::read(path)?);
-        match ext.as_str() {
-            "bmp" => Ok(Image::Bmp(Bmp::new(&mut reader)?)),
-            "gif" => Ok(Image::Gif(Gif::new(&mut reader)?)),
-            "jpeg" | "jpg" => Ok(Image::Jpeg(Jpeg::new(&mut reader)?)),
-            "png" => Ok(Image::Png(Png::new(&mut reader)?)),
-            "tiff" | "tif" => Ok(Image::Tiff(Tiff::new(&mut reader)?)),
+        let data = std::fs::read(path)?;
+        let crc32 = const_crc32::crc32(&data);
+        let mut reader = Cursor::new(data);
+        let r#type = match ext.as_str() {
+            "bmp" => Ok(ImageType::Bmp(Bmp::new(&mut reader)?)),
+            "gif" => Ok(ImageType::Gif(Gif::new(&mut reader)?)),
+            "jpeg" | "jpg" => Ok(ImageType::Jpeg(Jpeg::new(&mut reader)?)),
+            "png" => Ok(ImageType::Png(Png::new(&mut reader)?)),
+            "tiff" | "tif" => Ok(ImageType::Tiff(Tiff::new(&mut reader)?)),
             _ => Err(Error::CommonError("不支持的格式".to_string())),
-        }
-    }
+        }?;
 
+        Ok(Self {
+            crc32,
+            dimensions: r#type.dimensions(),
+            width: r#type.width(),
+            height: r#type.height(),
+            x_dpi: r#type.x_dpi(),
+            y_dpi: r#type.y_dpi(),
+            content_type: r#type.content_type().to_string(),
+            ext: r#type.default_ext().to_string(),
+        })
+    }
+}
+
+impl ImageType {
     pub fn content_type(&self) -> &'static str {
         match self {
-            Image::Bmp(_) => "image/bmp",
-            Image::Gif(_) => "image/gif",
-            Image::Jpeg(_) => "image/jpeg",
-            Image::Png(_) => "image/png",
-            Image::Tiff(_) => "image/tiff",
+            ImageType::Bmp(_) => "image/bmp",
+            ImageType::Gif(_) => "image/gif",
+            ImageType::Jpeg(_) => "image/jpeg",
+            ImageType::Png(_) => "image/png",
+            ImageType::Tiff(_) => "image/tiff",
         }
     }
 
     pub fn default_ext(&self) -> &'static str {
         match self {
-            Image::Bmp(_) => "bmp",
-            Image::Gif(_) => "gif",
-            Image::Jpeg(_) => "jpg",
-            Image::Png(_) => "png",
-            Image::Tiff(_) => "tiff",
+            ImageType::Bmp(_) => "bmp",
+            ImageType::Gif(_) => "gif",
+            ImageType::Jpeg(_) => "jpeg",
+            ImageType::Png(_) => "png",
+            ImageType::Tiff(_) => "tiff",
         }
     }
 
     pub fn dimensions(&self) -> (u32, u32) {
         match self {
-            Image::Bmp(r) => r.dimension(),
-            Image::Gif(r) => r.dimension(),
-            Image::Jpeg(r) => r.dimension(),
-            Image::Png(r) => r.dimension(),
-            Image::Tiff(r) => r.dimension(),
+            ImageType::Bmp(r) => r.dimension(),
+            ImageType::Gif(r) => r.dimension(),
+            ImageType::Jpeg(r) => r.dimension(),
+            ImageType::Png(r) => r.dimension(),
+            ImageType::Tiff(r) => r.dimension(),
         }
     }
 
     pub fn width(&self) -> Length {
         match self {
-            Image::Bmp(r) => r.width(),
-            Image::Gif(r) => r.width(),
-            Image::Jpeg(r) => r.width(),
-            Image::Png(r) => r.width(),
-            Image::Tiff(r) => r.width(),
+            ImageType::Bmp(r) => r.width(),
+            ImageType::Gif(r) => r.width(),
+            ImageType::Jpeg(r) => r.width(),
+            ImageType::Png(r) => r.width(),
+            ImageType::Tiff(r) => r.width(),
         }
     }
 
     pub fn height(&self) -> Length {
         match self {
-            Image::Bmp(r) => r.height(),
-            Image::Gif(r) => r.height(),
-            Image::Jpeg(r) => r.height(),
-            Image::Png(r) => r.height(),
-            Image::Tiff(r) => r.height(),
+            ImageType::Bmp(r) => r.height(),
+            ImageType::Gif(r) => r.height(),
+            ImageType::Jpeg(r) => r.height(),
+            ImageType::Png(r) => r.height(),
+            ImageType::Tiff(r) => r.height(),
         }
     }
 
     pub fn x_dpi(&self) -> u32 {
         match self {
-            Image::Bmp(r) => r.x_dpi(),
-            Image::Gif(r) => r.x_dpi(),
-            Image::Jpeg(r) => r.x_dpi(),
-            Image::Png(r) => r.x_dpi(),
-            Image::Tiff(r) => r.x_dpi(),
+            ImageType::Bmp(r) => r.x_dpi(),
+            ImageType::Gif(r) => r.x_dpi(),
+            ImageType::Jpeg(r) => r.x_dpi(),
+            ImageType::Png(r) => r.x_dpi(),
+            ImageType::Tiff(r) => r.x_dpi(),
         }
     }
 
     pub fn y_dpi(&self) -> u32 {
         match self {
-            Image::Bmp(r) => r.y_dpi(),
-            Image::Gif(r) => r.y_dpi(),
-            Image::Jpeg(r) => r.y_dpi(),
-            Image::Png(r) => r.y_dpi(),
-            Image::Tiff(r) => r.y_dpi(),
+            ImageType::Bmp(r) => r.y_dpi(),
+            ImageType::Gif(r) => r.y_dpi(),
+            ImageType::Jpeg(r) => r.y_dpi(),
+            ImageType::Png(r) => r.y_dpi(),
+            ImageType::Tiff(r) => r.y_dpi(),
         }
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -6,51 +6,36 @@ use binrw::{BinRead, BinReaderExt};
 use imagesize::gif::Gif;
 use imagesize::jpeg::{Jpeg, Segment};
 use imagesize::jpeg::Marker::APP1;
-use imagesize::png::Png;
+use imagesize::png::{Chunk, Info, Png};
 use imagesize::tiff::Tiff;
 
 #[test]
 fn test_bmp() {
     let bmp = Image::from_file("tests/images/bmp.bmp").unwrap();
     println!("{:?}", bmp);
-    let width = bmp.width();
-    let height = bmp.height();
-    let x_dpi = bmp.x_dpi();
-    let y_dpi = bmp.y_dpi();
-    println!("{:?}", (width.value(), height.value(), x_dpi, y_dpi));
 }
 
 #[test]
 fn test_gif() {
     let bmp = Image::from_file("tests/images/gif.gif").unwrap();
     println!("{:?}", bmp);
-    let width = bmp.width();
-    let height = bmp.height();
-    let x_dpi = bmp.x_dpi();
-    let y_dpi = bmp.y_dpi();
-    println!("{:?}", (width.value(), height.value(), x_dpi, y_dpi));
-}
 
+}
+#[derive(Debug, BinRead)]
+#[br(big)]
+#[br(magic = b"\x89PNG\x0D\x0A\x1A\x0A")]
+pub struct Pngf {
+    #[br(ignore)]
+    pub info: Option<Info>,
+}
 #[test]
 fn test_png() {
     let bmp = Image::from_file("tests/images/png.png").unwrap();
     println!("{:?}", bmp);
-    let width = bmp.width();
-    let height = bmp.height();
-    let x_dpi = bmp.x_dpi();
-    let y_dpi = bmp.y_dpi();
-    println!("{:?}", (width.value(), height.value(), x_dpi, y_dpi));
 }
 
 #[test]
 fn test_jpeg() {
     let bmp = Image::from_file("tests/images/jpeg.jpg").unwrap();
     println!("{:?}", bmp);
-    let width = bmp.width();
-    let height = bmp.height();
-    let x_dpi = bmp.x_dpi();
-    let y_dpi = bmp.y_dpi();
-    println!("{:?}", (width.value(), height.value(), x_dpi, y_dpi));
-
-    // let mut reader = Cursor::new(reader);
 }
